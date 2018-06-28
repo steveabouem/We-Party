b60443dba2e7ef708bb1dee36a63bc5d541e098d
steveabouem <steveabouem@gmail.com>
2018-06-28 00:06:55 +0000

    Initial components

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from "./components/HomePage.jsx";
+import LandingPage from "./components/LandingPage.jsx";
+
+class App extends Component {
+  render() {
+    return <LandingPage />
+  }
+}
+
+export default App;
---
+++ app/src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class HomePage extends React.Component {
+  render(){
+    return( 
+      <div className="landing-page">
+        <div className="white-box">
+          <h1> Welcome to WeParty </h1>
+          <p> LOREM IPSUM WELCOME </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HomePage
