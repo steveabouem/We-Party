e4e16cb808770e26488817e6f6dd8cb9fc8354e7
steveabouem <steveabouem@gmail.com>
2018-10-11 19:37:59 +0000

    update before local pull request

--- app/src/components/LandingPage.jsx
+++ app/src/components/LandingPage.jsx
@@ -5,7 +5,10 @@ import { Link } from "react-router-dom";
 class HomePage extends React.Component {
   render(){
     return( 
-      <div className="landing-page">
+        <div className="landing-page">
+        <div className="slider-divs"/>
+        <div className="slider-divs"/>
+        <div className="slider-divs"/>
         <div className="white-box">
               <h1> We-Party </h1>
               <p> Our goal? Allow people to organise group activities on one single platform.
