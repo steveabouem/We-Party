c19f6861e8c40c37eb8cbfccc898e34b4ded1c3f
steveabouem <steveabouem@gmail.com>
2018-11-21 00:08:40 +0000

    small changes

--- app/src/components/Navigation.jsx
+++ app/src/components/Navigation.jsx
@@ -29,11 +29,12 @@ function Navigation(props) {
     <div className={classes.root} id="navigation-class-converter">
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" >
             <MenuButton items={[{ref: "/activities", name:"My Activities"}, {ref: "/groups", name:"My Groups"}, {ref: "/tour", name:"Tour"}, {ref: "/home", name:"Home page"}]} button={"WeParty"}/>
+            <Link to="/home" style={{textDecoration: "none", color: "white", margin: "0.5em 1em 0 2em"}}>WeParty!</Link>
           </IconButton>
           <span style={{position: "absolute", right: "1%"}}>
-            <span className="display-name" style={{padding: "1em"}}> {(props.userInfo.userInfo.userInfo? "Guest" : props.userInfo.userInfo.name)}</span>
+            <span className="display-name" style={{padding: "1em"}} id="span-id"> {(props.userInfo.userInfo.userInfo? "Guest" : props.userInfo.userInfo.name)}</span>
             {(props.userInfo.userInfo.name? <Link to="/authenticate" className="link-primary" style={{padding: "0.5em"}}>Logout</Link>
             :
             <Link to="/authenticate" className="link-primary" style={{padding: "0.5em"}} onClick={props.logout}>Login</Link>)}
