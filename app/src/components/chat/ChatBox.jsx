46f45e978bc8f5209c8a52e934a2b826fa130761
steveabouem <steveabouem@gmail.com>
2018-12-23 00:47:39 +0000

    all msgs history loads. not live

--- app/src/components/chat/ChatBox.jsx
+++ app/src/components/chat/ChatBox.jsx
@@ -5,21 +5,31 @@ import { getMsgHistory } from "../../actions";
 import { connect } from "react-redux";
 
 class ChatBox extends React.Component {
+  componentDidMount() {
+    if(this.props.userInfo.chatInfo) {
+      this.props.getMsgHistory(this.props.userInfo.chatInfo.chatkey);
+      console.log('box props', this.props);
+    }
+  }
 
   render() {
+    
+    let key = 3.01;
     return (
       <div className="ChatBox">
         <ul className="chat-messages-list">
           <li>{this.props.userInfo.chatInfo? this.props.userInfo.chatInfo.room:null}</li>
-          {this.props.userInfo.messages.length > 0? this.props.userInfo.messages.map(msg =>{
-            let list = Object.keys(msg);
-            for( let key in list){
-              let content = msg[list[list.length-1]];
+          {this.props.userInfo.messages.length > 0?
+            Object.keys(this.props.userInfo.messages[0]).map(key => {
+              let list = Object.keys(this.props.userInfo.messages[0]),
+              msgs = this.props.userInfo.messages[0];
+              console.log('props-messaes-msg-index in msg', msgs[key]);
+              
+              let content = msgs[key];
 
               return (
-              <PastMessages msg={content}/>
+              <PastMessages key={key += 1} msg={content}/>
               )
-            }
           })
           :
           null
